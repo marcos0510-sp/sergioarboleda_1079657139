44c7bde05d52955604d0f7a27e695a798b321875
marcos <marcos.sierra01@usa.edu.co>
2026-03-02 07:54:16 -0500

    taller while

--- taller_10979657139.py
+++ taller_10979657139.py
@@ -3,6 +3,12 @@ if edad >= 18:
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
-    
+
     for i in range(5):
         print("Hola")
+
+        #bucle while
+        contador=1 
+        while contador <= 5:
+            print("numero: " + str(contador))
+            contador = contador + 1
